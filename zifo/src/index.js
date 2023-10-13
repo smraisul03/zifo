74e1485d96db761157c0370266aeb7f870258ac2
Tom Tran <145043486+tommytran9@users.noreply.github.com>
2023-10-13 11:12:40 -0400

    NavbarPages
    imported navbar and created pages

--- zifo/src/index.js
+++ zifo/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Nav, NavLink, NavMenu }
+    from "./NavbarElements";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +12,29 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const Navbar = () => {
+  return (
+      <>
+          <Nav>
+              <NavMenu>
+                  <NavLink to="/about" activeStyle>
+                      About
+                  </NavLink>
+                  <NavLink to="/contact" activeStyle>
+                      Contact Us
+                  </NavLink>
+                  <NavLink to="/blogs" activeStyle>
+                      Blogs
+                  </NavLink>
+                  <NavLink to="/sign-up" activeStyle>
+                      Sign Up
+                  </NavLink>
+              </NavMenu>
+          </Nav>
+      </>
+  );
+};
+
+export default Navbar;
